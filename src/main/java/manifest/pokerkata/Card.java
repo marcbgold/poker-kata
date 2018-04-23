fe639ebb8fd53db8fa34cdc8e7204cc96d7f5e96
Marc Gold <marcbgold2@gmail.com>
2018-04-23 12:22:04 -0400

    cardShouldHaveSuit test passes

--- src/main/java/manifest/pokerkata/Card.java
+++ src/main/java/manifest/pokerkata/Card.java
@@ -2,11 +2,14 @@ package manifest.pokerkata;
 
 public class Card {
 
+	private String suit;
+
 	public Card(String suit) {
+		this.suit = suit;
 	}
 
 	public Object getSuit() {
-		return null;
+		return suit;
 	}
 
 }
