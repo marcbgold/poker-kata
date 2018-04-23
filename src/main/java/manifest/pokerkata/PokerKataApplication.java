335e3a109e38642b8acbc5ccaec7fb3a80d5530d
Marc Gold <marcbgold2@gmail.com>
2018-04-23 12:04:50 -0400

    first commit

---
+++ src/main/java/manifest/pokerkata/PokerKataApplication.java
@@ -0,0 +1,12 @@
+package manifest.pokerkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerKataApplication.class, args);
+	}
+}
